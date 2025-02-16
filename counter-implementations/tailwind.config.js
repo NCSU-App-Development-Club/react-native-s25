01734e72ab7d2d0411519582de74552122984046
Brendon Hablutzel <booklvrbren@gmail.com>
2025-02-16 18:43:14 -0500

    create counter app impl

--- counter-implementations/tailwind.config.js
+++ counter-implementations/tailwind.config.js
@@ -4,19 +4,20 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    backgroundColor: {
-      ncsured: '#CC0000',
-      ncsured2: '#990000',
-      ncsuorange: '#D14905',
-      ncsuyellow: '#FAC800',
-      ncsugreen: '#6F7D1C',
-      ncsublue: '#427E93',
-      ncsuaqua: '#008473',
-      ncsuindigo: '#4156A1',
-      ncsuwhite: '#FFFFFF',
-      ncsublack: '#000000',
+    extend: {
+      backgroundColor: {
+        ncsured: '#CC0000',
+        ncsured2: '#990000',
+        ncsuorange: '#D14905',
+        ncsuyellow: '#FAC800',
+        ncsugreen: '#6F7D1C',
+        ncsublue: '#427E93',
+        ncsuaqua: '#008473',
+        ncsuindigo: '#4156A1',
+        ncsuwhite: '#FFFFFF',
+        ncsublack: '#000000',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
